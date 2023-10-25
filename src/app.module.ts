3fc9b4921498f39194f228798718861057919b6c
Peter Gamali <petrugamali@gmail.com>
2023-10-25 11:26:13 +0300

    Fixed bugs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,17 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { FirebaseModule } from './firebase/firebase.module';
 import { FirestoreModule } from './firestore/firestore.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule, AuthModule, FirebaseModule, FirestoreModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    FirebaseModule,
+    FirestoreModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    JwtModule.register({ global: true }),
+  ],
 })
 export class AppModule {}
