9c00c36610ec3ad378b2a59ff207e8ca5e8ca7bc
Peter Gamali <petrugamali@gmail.com>
2023-10-30 20:32:05 +0200

    Added the functionality to fetch a users personal data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { UserService } from './user.service';
+import { RequestWithAuth } from '../guards/auth/request-with-auth.interface';
+import { constructResponseJson } from '../lib/respones';
+import { InternalServerErrorHttpException } from '../api-http-exceptions/ApiHttpExceptions';
+import { AuthenticationGuard } from '../guards/auth/auth.guard';
 
-@Controller('user')
-export class UserController {}
+@UseGuards(AuthenticationGuard)
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('me')
+  async getUserPersonalInfo(@Req() requestWithAuth: RequestWithAuth) {
+    const {
+      user: { userId },
+    } = requestWithAuth;
+
+    const userInfo = await this.userService.getUserPersonalInfo(userId);
+
+    if (userInfo === null) throw new InternalServerErrorHttpException();
+
+    return constructResponseJson(userInfo);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './interface/user.interface';
+import { FirestoreService } from '../firestore/firestore.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private db: FirestoreService) {}
+
+  async getUserPersonalInfo(userId: User['userId']) {
+    const userInfo = await this.db.getUser(userId);
+
+    return userInfo;
+  }
+}
